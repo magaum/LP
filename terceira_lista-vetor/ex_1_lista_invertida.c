dacdabc3879167075f012519640c621c43da5443
weslei_luiz22 <weslei_luiz22@hotmail.com>
2017-04-06 08:50:16 -0400

    resolvido

--- terceira_lista-vetor/ex_1_lista_invertida.c
+++ terceira_lista-vetor/ex_1_lista_invertida.c
@@ -1,1 +1,28 @@
+#include <stdio.h>
 
+/*
+  ENUNCIADO
+  
+  Dada uma sequência de n números, imprimí-la na ordem inversa à da leitura.
+
+*/
+
+#define TAM 100
+int main(){
+  int i=0,k=0, numero,inverso, vetor[TAM];
+  
+  scanf("%d", & numero);
+  
+  while(numero > 0){
+    vetor[i]=0;
+    inverso=numero%10;
+    numero /= 10;
+    vetor[i]=inverso;
+    printf("Indice %d = %d\n", i, inverso);
+    i++;
+    k=i;
+  }
+  for (i=0; i<k;i++){
+    printf("%d\n",vetor[i]);
+  }
+}
