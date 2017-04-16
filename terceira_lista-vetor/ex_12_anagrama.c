e41865651efdc64dbcb325f0479c02b405f0e9ee
magaum <weslei_luiz22@hotmail.com>
2017-04-16 00:55:06 -0300

    DOIS VETORES

--- terceira_lista-vetor/ex_12_anagrama.c
+++ terceira_lista-vetor/ex_12_anagrama.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <locale.h>
+#include <string.h>
+
 /*
 
 	Leia dois vetores de n posições. Verifique e escreva se um é anagrama de outro.
@@ -9,22 +11,30 @@
 
 int main(){
 
-	char palavra[50];
-	int i,anagrama=-1,tamanho,j=0;
+	char palavra1[50],palavra2[50];
+	int i,anagrama=-1,tamanho1,tamanho2,j=0,flag=1;
 	
 	printf("Digite uma palavra: ");
-	scanf("%s",palavra);
-	(tamanho=strlen(palavra)-1);
-	
-	for (i=tamanho;i>=0;i--){
-		if(palavra[i]==palavra[j]){
-			anagrama++;
-		}
-		j++;
+	scanf("%s",palavra1);
+	(tamanho1 = strlen(palavra1)-1);
+	printf("Digite outra palavra: ");
+	scanf("%s",palavra2);
+	(tamanho2 = strlen(palavra2)-1);
+	if(tamanho1!=tamanho2){
+		flag=0;
+		printf("%s e %s não são um anagrama.\n",palavra1,palavra2);
 	}
-	if (anagrama == tamanho){
-		printf("%s é um anagrama.\n",palavra);
-	}else{
-		printf("%s não é um anagrama.\n",palavra);
+	else{
+		for (i=tamanho1;i>=0;i--){
+			if(palavra1[i]==palavra2[j]){
+				anagrama++;
+			}
+			j++;
+		}
+	}
+	if (anagrama == tamanho1){
+		printf("%s e %s são um anagrama.\n",palavra1,palavra2);
+	}else if(flag){
+		printf("%s e %s não são um anagrama.\n",palavra1,palavra2);
 	}
 }
