b3d34193580c785eaadff0652c4ae0240fdb1b2d
weslei_luiz22 <weslei_luiz22@hotmail.com>
2017-04-28 19:50:26 -0400

    testes das pontas

--- EP/zombie.c
+++ EP/zombie.c
@@ -1,22 +1,59 @@
 #include <stdio.h>
+#include <string.h>
 #define vetor 5
+
 int main(){
   
-  int i,j,aux,x,y,matriz[vetor][vetor],flag=0;
+  char matriz[vetor][vetor], xxx[vetor];
+  int x, y;
   
-    for (x=0;x<vetor;x++){
-       for(y=0;y<vetor;y++){
-         scanf("%d",&matriz[x][y]);
-       }
-    }
-    while (True){
-      for (x=0;x<vetor;x++){
-       for(y=0;y<vetor;y++){
-         if(matriz[x][y]==0){
-            
-           }
-         }
-       }
-    }
+ /* for (x=0;x<vetor;x++){
+    gets(xxx);
+      for(y=0; y<vetor ; y++){
+        matriz[x][y]=xxx[y];
+      }
+   }*/
   
+  for (x=0;x<vetor;x++){
+    for(y=0;y<vetor;y++){
+      //matriz[4][3]=3;matriz[3][4]=3;
+      if (x==0&&y==0){
+        matriz[x][y]=0;
+        if (matriz[0][1]==1&&matriz[1][0]==1){
+          matriz[x][y]=1;
+        }
+      }else if(x==0&&y==4){
+        matriz[x][y]=0;
+        if (matriz[0][3]==1&&matriz[1][4]==1){
+          matriz[x][y]=1;
+        }
+      }else if(x==4&&y==0){
+        matriz[x][y]=0;
+        if (matriz[3][0]==1&&matriz[4][1]==1){
+          matriz[x][y]=1;
+        }
+      }else if(x==4&&y==4){
+        matriz[x][y]=0;
+        if (matriz[4][3]==1&&matriz[3][4]==1){
+          matriz[x][y]=1;
+        }
+      }else if(x==0&&(y==1||y==2||y==3)){
+        matriz[x][y]=1;
+      }else if(x==4&&(y==1||y==2||y==3)){
+        matriz[x][y]=1;
+      }else if (y==0&&(x==1||x==2||x==3)){
+        matriz[x][y]=1;
+      }else if (y==4&&(x==1||x==2||x==3)){
+        matriz[x][y]=1;
+      }else{
+        matriz[x][y]=0;
+      }
+    }
+  }
+  
+  for (x=0;x<vetor;x++){
+    for(y=0;y<vetor;y++){
+      printf("%d", matriz[x][y]);
+    }printf("\n");
+  }
 }
