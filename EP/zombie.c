de3f8c5cfbc3f28879903f27bc7f7fe9fdbc5f32
weslei_luiz22 <weslei_luiz22@hotmail.com>
2017-04-29 23:32:35 -0400

    testes adicionados

--- EP/zombie.c
+++ EP/zombie.c
@@ -16,14 +16,15 @@ int main(){
   
   for (x=0;x<vetor;x++){
     for(y=0;y<vetor;y++){
-      //matriz[4][3]=3;matriz[3][4]=3;
+      //matriz[4][0]=3;
+      //matriz[4][2]=3;
+      //matriz[3][1]=3;
       if (x==0&&y==0){
         matriz[x][y]=0;
-        if (matriz[0][1]==1&&matriz[1][0]==1){
+        if (matriz[0][1]==3&&matriz[1][0]==3){
           matriz[x][y]=1;
         }
       }else if(x==0&&y==4){
-        matriz[x][y]=0;
         if (matriz[0][3]==1&&matriz[1][4]==1){
           matriz[x][y]=1;
         }
@@ -38,9 +39,13 @@ int main(){
           matriz[x][y]=1;
         }
       }else if(x==0&&(y==1||y==2||y==3)){
-        matriz[x][y]=1;
+        if (matriz[x][y-1]==1&&matriz[x][y+1]==1||matriz[x][y-1]==1&&matriz[x+1][y]==1||matriz[x][y+1]==1&&matriz[x+1][y]==1){
+          matriz[x][y]=1;
+        }
       }else if(x==4&&(y==1||y==2||y==3)){
-        matriz[x][y]=1;
+        if (matriz[x][y-1]==1&&matriz[x][y+1]==1||matriz[x][y-1]==1&&matriz[x-1][y]==1||matriz[x][y-1]==1&&matriz[x-1][y]==1){
+          matriz[x][y]=1;
+        }
       }else if (y==0&&(x==1||x==2||x==3)){
         matriz[x][y]=1;
       }else if (y==4&&(x==1||x==2||x==3)){
