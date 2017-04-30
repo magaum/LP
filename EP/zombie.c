38ea71add1988d40878d8d38e619e88f028df6e3
magaum <weslei_luiz22@hotmail.com>
2017-04-30 10:58:34 -0300

    testes finalizados, agora falta a contaminação

--- EP/zombie.c
+++ EP/zombie.c
@@ -4,61 +4,80 @@
 
 int main(){
   
-  char matriz[vetor][vetor], xxx[vetor];
-  int x, y;
+	char matriz[vetor][vetor], linhas[vetor], matriz_infectada[vetor][vetor];
+	int x, y, flag=1;
   
- /* for (x=0;x<vetor;x++){
-    gets(xxx);
-      for(y=0; y<vetor ; y++){
-        matriz[x][y]=xxx[y];
-      }
-   }*/
-  
-  for (x=0;x<vetor;x++){
-    for(y=0;y<vetor;y++){
-      //matriz[4][0]=3;
-      //matriz[4][2]=3;
-      //matriz[3][1]=3;
-      if (x==0&&y==0){
-        matriz[x][y]=0;
-        if (matriz[0][1]==3&&matriz[1][0]==3){
-          matriz[x][y]=1;
-        }
-      }else if(x==0&&y==4){
-        if (matriz[0][3]==1&&matriz[1][4]==1){
-          matriz[x][y]=1;
-        }
-      }else if(x==4&&y==0){
-        matriz[x][y]=0;
-        if (matriz[3][0]==1&&matriz[4][1]==1){
-          matriz[x][y]=1;
-        }
-      }else if(x==4&&y==4){
-        matriz[x][y]=0;
-        if (matriz[4][3]==1&&matriz[3][4]==1){
-          matriz[x][y]=1;
-        }
-      }else if(x==0&&(y==1||y==2||y==3)){
-        if (matriz[x][y-1]==1&&matriz[x][y+1]==1||matriz[x][y-1]==1&&matriz[x+1][y]==1||matriz[x][y+1]==1&&matriz[x+1][y]==1){
-          matriz[x][y]=1;
-        }
-      }else if(x==4&&(y==1||y==2||y==3)){
-        if (matriz[x][y-1]==1&&matriz[x][y+1]==1||matriz[x][y-1]==1&&matriz[x-1][y]==1||matriz[x][y-1]==1&&matriz[x-1][y]==1){
-          matriz[x][y]=1;
-        }
-      }else if (y==0&&(x==1||x==2||x==3)){
-        matriz[x][y]=1;
-      }else if (y==4&&(x==1||x==2||x==3)){
-        matriz[x][y]=1;
-      }else{
-        matriz[x][y]=0;
-      }
-    }
-  }
-  
-  for (x=0;x<vetor;x++){
-    for(y=0;y<vetor;y++){
-      printf("%d", matriz[x][y]);
-    }printf("\n");
-  }
+	for (x=0;x<vetor;x++){
+		gets(linhas);
+		for(y=0; y<vetor ; y++){
+			matriz[x][y]=linhas[y];
+		}
+	}
+//	while (flag){
+//		flag=0;
+		for (x=0;x<vetor;x++){
+			for(y=0;y<vetor;y++){
+				if (x==0&&y==0){//ponta superior esquerda
+					if (matriz[0][1]=='1'&&matriz[1][0]=='1'){
+						flag=1;
+						matriz[x][y]='1';
+					}
+				}
+				else if(x==0&&y==4){//ponta superior direita
+					if (matriz[0][3]=='1'&&matriz[1][4]=='1'){
+						flag=1;
+						matriz[x][y]='1';
+					}
+				}
+				else if(x==4&&y==0){//ponta inferior esquerda
+					if (matriz[3][0]=='1'&&matriz[4][1]=='1'){
+						flag=1;
+						matriz[x][y]='1';
+					}
+				}
+				else if(x==4&&y==4){//ponta inferior direita
+					if (matriz[4][3]=='1'&&matriz[3][4]=='1'){
+						flag=1;
+						matriz[x][y]='1';
+					}
+				}
+				else if(x==0&&(y==1||y==2||y==3)){//lateral superior
+					if (matriz[x][y-1]=='1'&&matriz[x][y+1]=='1' || matriz[x][y-1]=='1'&&matriz[x+1][y]=='1' || matriz[x][y+1]=='1'&&matriz[x+1][y]=='1'){
+						flag=1;
+						matriz[x][y]='1';
+					}
+				}
+				else if(x==4&&(y==1||y==2||y==3)){//lateral inferior
+					if (matriz[x][y-1]=='1'&&matriz[x][y+1]=='1' || matriz[x][y-1]=='1'&&matriz[x-1][y]=='1' || matriz[x][y-1]=='1'&&matriz[x-1][y]=='1'){
+						flag=1;
+						matriz[x][y]='1';
+					}
+				}
+				else if (y==0&&(x==1||x==2||x==3)){//lateral esquerda
+					if (matriz[x-1][y]=='1'&&matriz[x+1][y]=='1' || matriz[x][y+1]=='1'&&matriz[x-1][y]=='1' || matriz[x][y+1]=='1'&&matriz[x+1][y]=='1'){
+						flag=1;
+						matriz[x][y]='1';
+					}
+				}
+				else if (y==4&&(x==1||x==2||x==3)){//lateral direita
+					if (matriz[x-1][y]=='1'&&matriz[x+1][y]=='1' || matriz[x][y-1]=='1'&&matriz[x-1][y]=='1' || matriz[x][y-1]=='1'&&matriz[x+1][y]=='1'){
+						flag=1;
+						matriz[x][y]='1';
+					}
+				}
+				else{//meio
+					if (matriz[x-1][y]=='1'&&matriz[x+1][y]=='1' || matriz[x][y+1]=='1'&&matriz[x][y-1]=='1' || matriz[x][y-1]=='1'&&matriz[x-1][y]=='1' || matriz[x-1][y]=='1'&&matriz[x][y+1]=='1' || matriz[x][y+1]=='1'&&matriz[x+1][y]=='1' || matriz[x+1][y]=='1'&&matriz[x][y-1]=='1'){
+						flag=1;
+						matriz[x][y]='1';
+					}
+				}
+			}
+		}
+//	}
+	printf("\n");
+	for (x=0;x<vetor;x++){
+		for(y=0;y<vetor;y++){
+			printf("%c", matriz[x][y]);
+		}printf("\n");
+	}
 }
