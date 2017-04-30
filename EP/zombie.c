cdcb2db74ce52ca5e91b32a0a7de45dec2916c64
magaum <weslei_luiz22@hotmail.com>
2017-04-30 11:12:54 -0300

    contaminado!

--- EP/zombie.c
+++ EP/zombie.c
@@ -5,7 +5,7 @@
 int main(){
   
 	char matriz[vetor][vetor], linhas[vetor], matriz_infectada[vetor][vetor];
-	int x, y, flag=1;
+	int x, y, flag=5;
   
 	for (x=0;x<vetor;x++){
 		gets(linhas);
@@ -13,67 +13,57 @@ int main(){
 			matriz[x][y]=linhas[y];
 		}
 	}
-//	while (flag){
-//		flag=0;
+	while (flag){
 		for (x=0;x<vetor;x++){
 			for(y=0;y<vetor;y++){
 				if (x==0&&y==0){//ponta superior esquerda
 					if (matriz[0][1]=='1'&&matriz[1][0]=='1'){
-						flag=1;
 						matriz[x][y]='1';
 					}
 				}
-				else if(x==0&&y==4){//ponta superior direita
+				if(x==0&&y==4){//ponta superior direita
 					if (matriz[0][3]=='1'&&matriz[1][4]=='1'){
-						flag=1;
 						matriz[x][y]='1';
 					}
 				}
-				else if(x==4&&y==0){//ponta inferior esquerda
+				if(x==4&&y==0){//ponta inferior esquerda
 					if (matriz[3][0]=='1'&&matriz[4][1]=='1'){
-						flag=1;
 						matriz[x][y]='1';
 					}
 				}
-				else if(x==4&&y==4){//ponta inferior direita
+				if(x==4&&y==4){//ponta inferior direita
 					if (matriz[4][3]=='1'&&matriz[3][4]=='1'){
-						flag=1;
 						matriz[x][y]='1';
 					}
 				}
-				else if(x==0&&(y==1||y==2||y==3)){//lateral superior
+				if(x==0&&(y==1||y==2||y==3)){//lateral superior
 					if (matriz[x][y-1]=='1'&&matriz[x][y+1]=='1' || matriz[x][y-1]=='1'&&matriz[x+1][y]=='1' || matriz[x][y+1]=='1'&&matriz[x+1][y]=='1'){
-						flag=1;
 						matriz[x][y]='1';
 					}
 				}
-				else if(x==4&&(y==1||y==2||y==3)){//lateral inferior
+				if(x==4&&(y==1||y==2||y==3)){//lateral inferior
 					if (matriz[x][y-1]=='1'&&matriz[x][y+1]=='1' || matriz[x][y-1]=='1'&&matriz[x-1][y]=='1' || matriz[x][y-1]=='1'&&matriz[x-1][y]=='1'){
-						flag=1;
 						matriz[x][y]='1';
 					}
 				}
-				else if (y==0&&(x==1||x==2||x==3)){//lateral esquerda
+				if (y==0&&(x==1||x==2||x==3)){//lateral esquerda
 					if (matriz[x-1][y]=='1'&&matriz[x+1][y]=='1' || matriz[x][y+1]=='1'&&matriz[x-1][y]=='1' || matriz[x][y+1]=='1'&&matriz[x+1][y]=='1'){
-						flag=1;
 						matriz[x][y]='1';
 					}
 				}
-				else if (y==4&&(x==1||x==2||x==3)){//lateral direita
+				if (y==4&&(x==1||x==2||x==3)){//lateral direita
 					if (matriz[x-1][y]=='1'&&matriz[x+1][y]=='1' || matriz[x][y-1]=='1'&&matriz[x-1][y]=='1' || matriz[x][y-1]=='1'&&matriz[x+1][y]=='1'){
-						flag=1;
 						matriz[x][y]='1';
 					}
 				}
-				else{//meio
+				if (x!=0&&x!=4&&y!=0&&y!=4){//meio
 					if (matriz[x-1][y]=='1'&&matriz[x+1][y]=='1' || matriz[x][y+1]=='1'&&matriz[x][y-1]=='1' || matriz[x][y-1]=='1'&&matriz[x-1][y]=='1' || matriz[x-1][y]=='1'&&matriz[x][y+1]=='1' || matriz[x][y+1]=='1'&&matriz[x+1][y]=='1' || matriz[x+1][y]=='1'&&matriz[x][y-1]=='1'){
-						flag=1;
 						matriz[x][y]='1';
 					}
 				}
 			}
-		}
-//	}
+		}flag--;
+	}
 	printf("\n");
 	for (x=0;x<vetor;x++){
 		for(y=0;y<vetor;y++){
