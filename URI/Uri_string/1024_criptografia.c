94ec5d9a136d4e425eab15c4b6cfd520765557d1
weslei_luiz22 <weslei_luiz22@hotmail.com>
2017-05-05 23:22:44 -0400

    sinc

--- URI/Uri_string/1024_criptografia.c
+++ URI/Uri_string/1024_criptografia.c
@@ -6,31 +6,32 @@ void limparBuffer(void);
 int main(){
 
 	int N,i,j,k,inverte;
-	char palavra[1000],palavra_invertida[1000];
+	char palavra[1000];
 
 	scanf("%d",&N);
 	limparBuffer();
 	for (i=0;i<N;i++){
-//		scanf("%s",palavra);
+		//gets(palavra);
 		fgets(palavra,1000,stdin);
-/*		while (palavra[k]!='\0'){
-			if (palavra[k]!=' ')
-		}*/
+		//scanf("%s",palavra);
+		inverte=strlen(palavra);
+		printf("%d\n",inverte);
 		for (j=0,inverte=strlen(palavra);j<strlen(palavra);j++,inverte--){
 			if (palavra[j]>=65 && palavra[j]<=90 || palavra[j]>=97 && palavra[j]<=122){
 				k=palavra[j];
-				k+=3;
+				//printf("%d %d",j,inverte);
+				/*if (j<(strlen(palavra)/2)){
+					k+=2;
+				}else{
+					k+=3;
+				}*/
 				palavra[j]=k;
-				printf("%d %d\n",inverte,j);
-				palavra_invertida[inverte]=palavra[j];
 			}
-//			printf("%c",palavra[j]);
 		}
-		for (j=0;j<strlen(palavra);j++){
-			printf("%c",palavra_invertida[j]);
+		for (j=strlen(palavra);j>=0;j--){
+			printf("%c",palavra[j]);
 		}printf("\n");
 	}
-
 }
 
 void limparBuffer(void){
