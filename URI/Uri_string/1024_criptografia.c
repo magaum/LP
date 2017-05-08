3b8858fa0dd98d55f2873d40ad8bb2fa88771848
magaum <weslei_luiz22@hotmail.com>
2017-05-08 02:06:58 -0300

    agora falta só tirar os strlen

--- URI/Uri_string/1024_criptografia.c
+++ URI/Uri_string/1024_criptografia.c
@@ -3,15 +3,14 @@
 
 int main(){
 
-	int N,i,j,k,inverte,flag=0;
+	int N,i,j,k,inverte;
 	char palavra[1001];
 
 	scanf("%d",&N);
 	getchar();
 	for (i=0;i<N;i++){
 		gets(palavra);
-		inverte=strlen(palavra);
-		for (j=0,inverte;j<inverte;j++,inverte--){
+		for (j=0,inverte=strlen(palavra);j<strlen(palavra);j++,inverte--){
 			k=palavra[j];
 			if (palavra[j]>=65 && palavra[j]<=90 || palavra[j]>=97 && palavra[j]<=122){
 					k+=3;
