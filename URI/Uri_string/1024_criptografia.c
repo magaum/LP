fbb82e04861bd2cb13259dbe11a6c39959da6213
magaum <weslei_luiz22@hotmail.com>
2017-05-08 01:31:40 -0300

    tem que tirar os strlen

--- URI/Uri_string/1024_criptografia.c
+++ URI/Uri_string/1024_criptografia.c
@@ -1,40 +1,36 @@
 #include <stdio.h>
 #include <string.h>
 
-void limparBuffer(void);
-
 int main(){
 
 	int N,i,j,k,inverte,flag=0;
-	char palavra[1000];
+	char palavra[1001];
 
 	scanf("%d",&N);
-	limparBuffer();
+	getchar();
 	for (i=0;i<N;i++){
 		gets(palavra);
-		//fgets(palavra,1000,stdin);
-		//scanf("%s",palavra);
-		inverte=(strlen(palavra)/2);
-		for (j=0,inverte=strlen(palavra);j<strlen(palavra);j++,inverte--){
+		inverte=strlen(palavra);
+		for (j=0,inverte;j<inverte;j++,inverte--){
 			k=palavra[j];
-			if (j<inverte){
-				if (k>=65&&k<=90||k>=97&&k<=122){
-					k+=2;
-				}else{
-					k+=-1;
-				}
-			}else if (palavra[j]>=65 && palavra[j]<=90 || palavra[j]>=97 && palavra[j]<=122){
+			if (palavra[j]>=65 && palavra[j]<=90 || palavra[j]>=97 && palavra[j]<=122){
 					k+=3;
 			}
 			palavra[j]=k;
 		}
-		for (j=strlen(palavra);j>=0;j--){
+		inverte=(strlen(palavra)/2);
+		if (strlen(palavra)%2!=0){
+			inverte++;
+		}
+
+		for (j=0;j<inverte;j++){
+			k=palavra[j];
+			k--;
+			palavra[j]=k;
+		}
+
+		for (j=strlen(palavra)-1;j>=0;j--){
 			printf("%c",palavra[j]);
 		}printf("\n");
 	}
-}
-
-void limparBuffer(void){
-		char c;
-			while((c=getchar()) != '\n' && c != EOF);
 }
