22042a0dbcc25776ca432752aa9fadcc5ab4b8e3
weslei_luiz22 <weslei_luiz22@hotmail.com>
2017-05-15 07:32:59 -0400

    tava errado rapaz

--- Ponteiros/media_valor.c
+++ Ponteiros/media_valor.c
@@ -18,17 +18,18 @@ int main(){
 
 void media_moda(int *media, int *moda, int tam, int vetor[]){
 
-        int soma=0,i,j,maior=0;
+        int soma=0,i,j,maior=0,aux=0;
   
         for (i=0;i<tam;i++){
               soma+=vetor[i];
               for(j=0;j<tam;j++){
                       if(vetor[i]==vetor[j]){
                             maior++;
-                            if(maior>*moda){
-                                  *moda=vetor[i];
-                            }
                       }
+              }
+              if(maior>aux){
+                 aux=maior;
+                 *moda=vetor[i];
               }
               maior=0;
         }
