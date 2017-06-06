69b221b2d54927a0d27b2e827f77cf7d4d66fba9
magaum <weslei_luiz22@hotmail.com>
2017-06-06 08:48:06 -0300

    sinc

--- Struct/Fracao.c
+++ Struct/Fracao.c
@@ -5,29 +5,33 @@ struct fracao{
   int num;  
 };
 
-/*void exibefracao(fracaoA);
-struct fracao somafracao(fracaoA, fracaoB);
+void exibefracao(struct fracao fracaoA);
+struct fracao somafracao(fracaoA, fracaoB);/*
 struct fracao multiplica(fracaoA, fracaoB);
 struct fracao subtrai(fracaoA, fracaoB);*/
 
 int main(){
   struct fracao x;
-  printf("Digite a fracao: \nNumerador:     \t");
+  struct fracao y;
+  printf("Digite a fracao 1: \nNumerador: ");
   scanf("%d",&x.num);
-  printf("\t\t---\n");
-  printf("Denominador:  \t");
+  printf("Denominador: ");
   scanf("%d",&x.den);
+  printf("Digite a fracao 2: \nNumerador: ");
+  scanf("%d",&y.num);
+  printf("Denominador: ");
+  scanf("%d",&y.den);
   
-  x=exibefracao(x);
-  printf("%d\n---%d",)
+  exibefracao(x);
+}
+
+void exibefracao(struct fracao fracaoA){
+  printf("%d/%d\n",fracaoA.num,fracaoA.den);
+}
+struct fracao somafracao(struct fracaoA, struct fracaoB){
+  
 }
 /*
-void exibefracao(fracaoA){
-  
-}
-struct fracao somafracao(fracaoA, fracaoB){
-  
-}
 struct fracao multiplica(fracaoA, fracaoB){
   
 }
