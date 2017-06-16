004d745565749e2d3f710abcb328a4b517875009
weslei_luiz22 <weslei_luiz22@hotmail.com>
2017-06-16 18:28:55 -0400

    deu certo birl

--- Arquivos/conta_palavras.c
+++ Arquivos/conta_palavras.c
@@ -4,19 +4,33 @@ int main(){
   
   FILE *in;
   
-  int c,palavras=0,flag_espaco=1,flag_caracter=0;
+  int c,palavras=0,flag_espaco=0,flag_caracter=0,verifica_caracter=0,soma_palavra=0;
   char * nome;
   printf("Digite o nome do arquivo: ");
   scanf("%s",nome);
   if (nome!=NULL){
     in = fopen(nome,"r");
     while((c=getc(in))!=EOF){
-      if(c==' '){
-        flag_espaco=0;
+      soma_palavra=0;
+      if(c!=' '){
+        flag_caracter++;
+      }
+      if(c==' '&&flag_caracter){
+        flag_espaco=1;
         palavras++;
+        soma_palavra=1;
+      }
+      if(flag_espaco&&c!=' '){
+        verifica_caracter++;
+      }
     }
-    if(flag_espaco){
-      palavras=0;
+    if (flag_caracter&&flag_espaco&&verifica_caracter&&soma_palavra){
+      palavras--;
+    }
+    if(flag_caracter&&flag_espaco&&verifica_caracter){
+      palavras++;
+    }else if(flag_caracter&&!flag_espaco){
+      palavras++;
     }
     printf("O arquivo tem %d palavras\n",palavras);
     fclose(in);
